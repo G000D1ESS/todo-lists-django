3d2c8e43c5653037b13cda8fec21b631134ccf7b
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-18 13:34:51 +0300

    Refactor functional tests

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.page_source
+class NewVisitorTest(unittest.TestCase):
+    ''' Тест от лица нового посетителя '''
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_stat_a_list_and_retrieve_it_later(self):
+        '''Тест: можно создать список и получить его позже'''
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
