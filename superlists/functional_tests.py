43c32cadb1ba21bef132fafd154a28141eefcb7b
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-18 17:35:41 +0300

    Functional tests now check ability to add a list element

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
+import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,9 +16,42 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_stat_a_list_and_retrieve_it_later(self):
         '''Тест: можно создать список и получить его позже'''
+
+        # Семён слышал про крутое онлайн-приложение со списком
+        # неотложных дел. Он решает оценить его домашнюю страницу
         self.browser.get('http://localhost:8000')
+
+        # Он видит, что заголовок и шапка страницы говорят о списках
+        # неотложныхх дел
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Ему сразу же предлагают ввести элемент из списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # Он набирает в текстовом поле - "Купить книгу по программированию"
+        inputbox.send_keys('Купить книгу по программированию')
+
+        # Когда он нажмёт Enter, страница обновляется, и теперь страница
+        # Содержит "1: Купить книгу по программированию" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить книгу по программированию' for row in rows)
+        )
+
+        # Текстовое поле по-прежнему приглашает добавить ещё один элемент.
+        # Он вводит "Написать первую программу"
         self.fail('Закончить тест!')
+        ...
 
 
 if __name__ == '__main__':
