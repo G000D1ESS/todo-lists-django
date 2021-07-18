3cdadb616c39500d04c4828f0dbc4a1c4c7bb488
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-18 16:24:50 +0300

    First module test and fictive Home page view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,10 +1,14 @@
+from django.urls import resolve
 from django.test import TestCase
 
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    '''Тест на токсичность'''
 
-    def test_bad_maths(self):
-        '''Тест: Неправильные математические расчёты'''
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    '''Тест домашней страницы'''
+
+    def test_root_url_resolves_to_home_page_view(self):
+        '''Тест: Кореновой URL преобразуется в представление домашней страницы'''
+        found = resolve('/').func
+        self.assertEqual(found, home_page)
 
