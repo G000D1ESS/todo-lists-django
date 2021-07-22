0874477a14dec6be7a496243d80a52de420dc0e3
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-22 23:00:35 +0300

    Redirect after POST-request and show all items in template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -18,8 +18,30 @@ class HomePageTest(TestCase):
     def test_can_save_a_POST_request(self):
         '''Тест: Можно сохранить POST-запрос'''
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        '''Тест: переадресует после POST-запроса'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        '''Тест: сохраняет элементы, только когда нужно'''
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        '''Тест: Отображаются все элементы списка'''
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -41,5 +63,5 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item, 'Item the second')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+
+from lists.models import Item
 
 
 def home_page(request):
     '''Домашняя страница'''
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),    
-    })
-
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
