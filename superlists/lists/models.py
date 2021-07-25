60bfc27c56054657ac68f03016f59e134b365f12
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-25 16:47:24 +0300

    Clean up lists/models.py

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -3,11 +3,9 @@ from django.db import models
 
 class List(models.Model):
     '''Список дел'''
-    pass
 
 
 class Item(models.Model):
     '''Элемент списка'''
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
-
