cdcd30b61c0142de305cde2e0ebb078b7099f99d
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-25 15:00:39 +0300

    Refactor module test + new TestCase for adding items to a List

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -53,19 +53,27 @@ class ListViewTest(TestCase):
     
     def test_uses_list_template(self):
         '''Тест: Используется шаблон списка'''
-        response = self.client.get('/lists/unique-list/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        '''Тест: Отображются все элементы списка'''
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+    def test_dislays_only_items_for_that_list(self):
+        '''Тест: Отображются элементы только для этого списка'''
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
 
-        response = self.client.get('/lists/unique-list/')
+        other_list = List.objects.create()
+        Item.objects.create(text='other item 1', list=other_list)
+        Item.objects.create(text='other item 2', list=other_list)
+        
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other item 1')
+        self.assertNotContains(response, 'other item 2')
+
 
 class NewListTest(TestCase):
     '''Тест нового списка'''
@@ -80,5 +88,36 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         '''Тест: переадресация полсле POST-запроса'''
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/unique-list/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+
+class NewItemTest(TestCase):
+    '''Тест нового элемента списка'''
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        '''Тест: можно сохранить POST-запрос в существующий список'''
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'},
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        '''Тест: переадресация в представление списика'''
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'},
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
