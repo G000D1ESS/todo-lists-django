0ef1708795aced3ff0f5b14699eedcf5077d14ac
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-25 16:43:52 +0300

    Refactor Lists urls

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/<int:list_id>/', views.view_list, name='view_list'),
-    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
+    path('', list_views.home_page, name='home'),
+    path('lists/', include(list_urls)),
 ]
