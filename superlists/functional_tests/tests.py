d3dbfba9d1de51761228186167105858d401396a
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-26 00:26:57 +0300

    Refactor List page view

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,7 +1,7 @@
 import time
 import unittest
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -9,7 +9,7 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     ''' Тест от лица нового посетителя '''
 
     def setUp(self):
