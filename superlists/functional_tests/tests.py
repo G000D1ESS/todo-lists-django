4e0170e0208f39d5ea2b904c2daf410225ff36f8
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-25 18:52:30 +0300

    First steps to test Website Layout and CSS

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -111,3 +111,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Купить молоко', page_text)
 
         # Семён и Артём заканчивают свою работу со списком
+
+    def test_layout_and_styling(self):
+        '''Тест макета и стилевого оформления'''
+        # Семён открывает домащнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Он замечает, что поле ввода центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10,
+        )
+
+        # Он начинает новый список и видит, что поле ввода всё ещё центрировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10,
+        )
