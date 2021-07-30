358f3faec8194d6f5e1a7e328d4994cb8b75b032
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-07-31 00:40:53 +0300

    Manage db.sqlite3 to  folder + change Time zobe to Europe/Moscow

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../database/db.sqlite3',
     }
 }
 
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
