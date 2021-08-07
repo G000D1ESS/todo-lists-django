e9cc8a0885feb7b9f5d136f5649f0ef2a7d66204
G000D1ESS <62620930+G000D1ESS@users.noreply.github.com>
2021-08-07 14:03:24 +0300

    Divided unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -16,38 +16,6 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ListAndItemModelTest(TestCase):
-    '''Тест модели элемента списка и самого списка'''
-
-    def test_saving_and_retrieving_items(self):
-        '''Тест сохранения и получения элементов списков'''
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-        
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_) 
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_) 
-
-
 class ListViewTest(TestCase):
     '''Тест представления списка'''
     
